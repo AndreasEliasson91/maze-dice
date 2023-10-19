8ffea038fb09e392c23a69603a9e294113a43a3d
Andreas Eliasson <andreaseliasson91@gmail.com>
2023-10-19 16:43:44 +0200

    added constructors etc. for Item

--- src/game-engine/assets/item.cpp
+++ src/game-engine/assets/item.cpp
@@ -1,13 +1,77 @@
 #include "item.h"
 
 
-// Item::Item(std::string label, std::string description, std::vector<std::string> actions, std::string storage, std::pair<int, int> pos)
-//     : label {label}, description {description}, storage {storage}, position {pos}
-//     {
-//         for (const auto &action : actions)
-//             this->actions.push_back(action);
-//     }
+Item::Item(std::string label, std::string description, std::vector<std::string> actions, std::string storage, std::pair<int, int> pos, bool container)
+    : label {label}, description {description}, storage {storage}, position {pos}
+{
+    for (const auto &action : actions)
+        this->actions.push_back(action);
+    
+    if (container) // TODO: Remove the hard-coded value
+        contains = new std::vector<Item*>;
+    else
+        contains = nullptr;
+}
 
+Item::Item(const Item& other)
+    : label(other.label), description(other.description), storage(other.storage), actions(other.actions), 
+      position(other.position), container(other.container)
+{
+    if (other.contains)
+    {
+        contains = new std::vector<Item*>;
+
+        for (Item* item : *(other.contains))
+            contains->push_back(new Item(*item));
+    }
+    else
+        contains = nullptr;
+}
+
+Item::Item(Item&& other) noexcept
+    : label {std::move(other.label)}, description {std::move(other.description)}, storage {std::move(other.storage)},
+      actions {std::move(other.actions)}, position {std::move(other.position)}, container {other.container}, contains {other.contains}
+{
+    other.contains = nullptr;
+}
+
+Item::~Item()
+{
+    if (contains)
+    {
+        for (Item* item : *contains)
+            delete item;
+
+        delete contains;
+    }
+}
+
+
+Item& Item::operator=(const Item& other)
+{
+    if (this != &other)
+    {
+        label = other.label;
+        description = other.description;
+        storage = other.storage;
+        actions = other.actions;
+        position = other.position;
+        container = other.container;
+
+        if (other.contains)
+        {
+            contains = new std::vector<Item*>;
+
+            for (Item* item : *(other.contains))
+                contains->push_back(new Item(*item));
+
+        } 
+        else
+            contains = nullptr;
+            
+    }
+    return *this;
+}
 
 // std::string inspect_item(const Cell &current_location, std::string label)
 // {
--- src/game-engine/assets/item.h
+++ src/game-engine/assets/item.h
@@ -7,8 +7,13 @@
 class Item
 {
 public:
-    Item(std::string label, std::string description, std::vector<std::string> actions, std::string storage, std::pair<int, int> pos);
-    ~Item() = default;
+    Item(std::string label, std::string description, std::vector<std::string> actions, std::string storage, std::pair<int, int> pos, bool container = false);
+    ~Item();
+
+    Item(const Item& other);
+    Item(Item&& other) noexcept;
+
+    Item& operator=(const Item& other);
 
     std::string get_label() const { return label; }
     std::string get_description() const { return description; }
@@ -19,13 +24,10 @@ private:
     std::string label, description, storage;
     std::vector<std::string> actions;
     std::pair<int, int> position;
+    bool container;
+    std::vector<Item*> *contains; // If chest etc.
 
 };
-
-// class Chest
-// {
-
-// };
 
 // TODO: Move this to a item_utils.h?
 // std::string inspect_item(const Cell &current_location, std::string label);
